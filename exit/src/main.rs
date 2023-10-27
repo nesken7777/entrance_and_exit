2c17efbcc35c9ee3682de4071bb24e6a60ec3b53
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2023-10-27 12:47:56 +0900

    exit完了
    まあentranceが完了しているので、大体コピペできる

--- exit/src/main.rs
+++ exit/src/main.rs
@@ -1,21 +1,49 @@
-
 use pcsc::{Context, Protocols, Scope, ShareMode, MAX_BUFFER_SIZE};
 use std::{
+    env::args,
+    ffi::CStr,
     fs::OpenOptions,
     io::{BufWriter, Write},
+    net::Ipv4Addr,
+    str::FromStr,
+    sync::{Mutex, OnceLock},
     thread,
     time::Duration,
 };
-fn main() {
-    const N: usize = 200;
 
+static mut READERS_BUF: [u8; 2048] = [0; 2048];
+static IDM_BEFORE: Mutex<u64> = Mutex::new(0);
+static IP_ADDRESS: OnceLock<Ipv4Addr> = OnceLock::new();
+
+fn main() {
+    let ip_arg = args().collect::<Vec<_>>();
+    let ip_arg = ip_arg.get(1).expect("IPアドレス欲しいですよー");
+    IP_ADDRESS
+        .get_or_init(|| Ipv4Addr::from_str(&ip_arg).expect("有効なIPアドレスじゃないですよー"));
     let context = Context::establish(Scope::User).unwrap();
-    let mut readers_buf = [0; 2048];
-    let mut readers = context.list_readers(&mut readers_buf).unwrap();
-    let mut idm_before = 0u64;
-    let reader = readers.nth(0).unwrap();
-    println!("使用するカードリーダ: {reader:?}");
+    let mut readers = context.list_readers(unsafe { &mut READERS_BUF }).unwrap();
+    let reader1 = readers.next();
+    let reader2 = readers.next();
+    println!("使用するカードリーダ: {reader1:?}");
+    println!("使用するカードリーダ: {reader2:?}");
     let _ = context.release();
+    let thread1;
+    let thread2;
+    if let Some(reader1) = reader1 {
+        thread1 = thread::spawn(move || exit(reader1));
+    } else {
+        thread1 = thread::spawn(|| {});
+    }
+    if let Some(reader2) = reader2 {
+        thread2 = thread::spawn(move || exit(reader2));
+    } else {
+        thread2 = thread::spawn(move || {});
+    }
+    let _ = thread1.join();
+    let _ = thread2.join();
+}
+
+fn exit(reader: &CStr) {
     loop {
         let context = Context::establish(Scope::User).unwrap();
 
@@ -36,33 +64,39 @@ fn main() {
         let result = &rapdu[len - 2..len];
 
         if !(*result.get(0).unwrap() == 0x90 && *result.get(1).unwrap() == 0x00) {
-            eprintln!("IDmの読み出しに失敗しました。")
+            eprintln!("IDmの読み出しが失敗したよー")
         } else {
+            let idm = &rapdu[..len - 2];
+            let Ok(idm_bytes) = idm.try_into() else {
+                eprintln!("IDmが64bitじゃないよー");
+                thread::sleep(Duration::from_millis(300));
+                continue;
+            };
+            let idm = u64::from_be_bytes(idm_bytes);
+            if idm != *IDM_BEFORE.lock().expect("Mutexのエラーは考えてない") {
+                *IDM_BEFORE.lock().expect("Mutexのエラーは考えてない") = idm;
+                let url = format!("http://127.0.0.1/exit.php?idm={}", idm);
+                reqwest::blocking::get(url).map_or_else(
+                    |_| {
+                        eprintln!("ネットワークエラーあるよ");
+                    },
+                    |_| {},
+                )
+            }
             let file = OpenOptions::new()
                 .write(true)
                 .create(true)
-                .open("IDm.txt")
+                .open("exit.txt")
                 .unwrap_or_else(|e| panic!("ファイル作成の時点でエラー起きたんやけど:{}", e));
             let mut buff = BufWriter::new(file);
-
-            let idm = &rapdu[..len - 2];
-            let idm = u64::from_be_bytes(idm.try_into().expect("え？IDm64bitじゃないの?"));
-
-            if idm != idm_before {
-                idm_before = idm;
-
-                let url = "http://127.0.0.1/exit.php?";
-                let url =  url.to_string() + "idm=" + &idm.to_string();
-                println!("{url}");
-                let body = reqwest::blocking::get(url).expect("ネットワークerror");
-                
-                
-                //request
-            }
-            buff.write(idm.to_string().as_bytes())
-                .unwrap_or_else(|e| panic!("??? :{}", e));
-            buff.flush()
-                .unwrap_or_else(|e| panic!("フラッシュ失敗した(さすがになくね？): {}", e));
+            buff.write(format!("{}\nexit\n", idm).as_bytes())
+                .unwrap_or_else(|e| {
+                    eprintln!("ファイル書き込みエラーって何??? :{}", e);
+                    0
+                });
+            buff.flush().unwrap_or_else(|e| {
+                eprintln!("フラッシュ失敗した(さすがになくね？): {}", e);
+            });
         }
 
         let _ = context.release();
